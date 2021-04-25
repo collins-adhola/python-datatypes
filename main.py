af1aa921889bacbe2e1e60d9c08a25618190ff4f
Collins Adhola <>
2021-04-25 20:16:26 +0000

    Added conditionals

--- main.py
+++ main.py
@@ -2,3 +2,44 @@ s = "This is a string"
 print(s)
 print(type(s))
 print(s[4:])
+#conditionals=======
+
+# x = 5
+# y = 9
+# if x < y:
+#   print('yes')
+
+#====================================
+# if 'hello' in 'hello world':
+#   print('yeah')
+
+#=====================================
+# x = 19
+# y = 19
+# if x < y:
+#  print('it is less')
+# elif x > y:
+#  print('It is less')
+# else: print('neither of the two')
+
+#======Nested IF===============
+# n = -2
+# if n>=0:
+#   if n==0:
+#     print('It is Zero')
+
+#   else:
+#        print('it is a positive number')
+# else:
+#       print('It is negative')
+
+#=======One line IF Statement=================
+b = 3
+if b > 2: print('Print it aint')     
+
+#=========Ternary operator==================
+print('YES INDEED') if b > 4 else print('NO INDEED')
+
+#======no action IF=======================
+if b < 2:
+  pass
